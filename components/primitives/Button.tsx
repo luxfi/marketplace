12a0937b6b061eece778f7987e1814a7ac11b723
Zach Kelling <z@zeekay.io>
2023-01-21 15:03:46 -0600

    Fix secondary colors

--- components/primitives/Button.tsx
+++ components/primitives/Button.tsx
@@ -36,11 +36,11 @@ const Button = styled('button', {
       },
       secondary: {
         backgroundColor: '$accent1',
-        border: '0px solid $primary1',
+        border: '2px solid $primary1',
         color: '$primary1',
         '&:hover': {
           backgroundColor: '$primary1',
-          border: '2px solid $accent1',
+          border: '2px solid $primary1',
           color: '$accent1',
         },
       },
