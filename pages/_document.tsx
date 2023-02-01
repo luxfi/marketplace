e7a81e4d5192f8c0089994ab7d88d2c04336ff9e
Zach Kelling <z@zeekay.io>
2023-02-01 16:48:08 -0600

    Add analytics code

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -17,9 +17,32 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
+          <script async src="https://www.googletagmanager.com/gtag/js?id=G-M5131VBRN3"></script>
           <style
             id="stitches"
             dangerouslySetInnerHTML={{ __html: getCssText() }}
+          />
+          <script
+          dangerouslySetInnerHTML={{
+            __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-M5131VBRN3');
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '1192795114810136');
+            fbq('track', 'PageView');
+            window.heap=window.heap||[],heap.load=function(e,t){window.heap.appid=e,window.heap.config=t=t||{};var r=document.createElement("script");r.type="text/javascript",r.async=!0,r.src="https://cdn.heapanalytics.com/js/heap-"+e+".js";var a=document.getElementsByTagName("script")[0];a.parentNode.insertBefore(r,a);for(var n=function(e){return function(){heap.push([e].concat(Array.prototype.slice.call(arguments,0)))}},p=["addEventProperties","addUserProperties","clearEventProperties","identify","resetIdentity","removeEventProperty","setEventProperties","track","unsetEventProperty"],o=0;o<p.length;o++)heap[p[o]]=n(p[o])};
+            heap.load("2120523489");
+          `,
+          }}
           />
         </Head>
 
@@ -27,6 +50,7 @@ class MyDocument extends Document {
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta name="keywords" content="blockhain, crypto, nft, ethereum, protocol, real world assets, RWAs" />
+        <meta name="facebook-domain-verification" content="2oe8vdr5jorny6ifjtixughdx1lnrr" />
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
@@ -103,6 +127,7 @@ class MyDocument extends Document {
           <Main />
           <NextScript />
         </body>
+        <noscript><img height="1" width="1" style={{display: 'none'}} src="https://www.facebook.com/tr?id=1192795114810136&ev=PageView&noscript=1"/></noscript>
       </Html>
     )
   }
